fd7135cf1d7f0215a730789c7a6d0a08f5e7df8b
hosenA51 <hosenahmed2552@gmail.com>
2024-10-20 03:06:23 +0600

    deploy live link by netlify

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-import CountryDetail from '../Components/CountryDetail/CountryDetail';
+import CountryDetail from '../CountryDetail/CountryDetail';
 const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
     const {name, flags, population, area, cca3} = country;
 
